ddb7efe1366a74061bd2009e647b580f83d5830b
imxood <imxood@163.com>
2021-11-16 21:30:21 +0800

    .

--- examples/test-shape.rs
+++ examples/test-shape.rs
@@ -51,59 +51,82 @@ impl Application for MyApp {
     }
 
     fn view(&mut self) -> Element<Message> {
-        let canvas = Canvas::new(Grid::new())
-            .width(Length::Fill)
-            .height(Length::Fill);
+        let canvas = Column::<Message>::new()
+            .width(Length::FillPortion(70))
+            .height(Length::Fill)
+            .spacing(20)
+            .push(
+                Canvas::new(Grid::new())
+                    .width(Length::Fill)
+                    .height(Length::Fill),
+            );
 
         let control = Column::<Message>::new()
-            .max_width(400)
+            .width(Length::FillPortion(30))
+            .height(Length::Fill)
             .spacing(20)
             .push(Text::new("hello, the world").size(50));
 
-        let first = Container::new(canvas)
+        let row = Row::<Message>::new()
             .width(Length::Fill)
-            .height(Length::Fill);
+            .height(Length::Fill)
+            .push(canvas)
+            .push(control)
+            .align_items(Align::End);
 
-        let second = Container::new(control)
-            .width(Length::FillPortion(400))
-            .height(Length::Fill);
+        let container: Element<_> = Container::new(row)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into();
 
-        let split: Element<_> =
-            Split::new(&mut self.split_pane, first, second, Message::OnResize).into();
-
-        split.explain(Color::from_rgb8(0x00, 0x00, 0xff))
+        // container.explain(Color::from_rgb8(0x00, 0x00, 0xff))
+        container
     }
 }
 
 struct Grid {
     row: i32,
     col: i32,
-    node_size: f32,
     interval: f32,
 }
 
 impl Grid {
     pub fn new() -> Self {
         Self {
-            row: 120,
-            col: 200,
-            node_size: 3.,
-            interval: 1.,
+            row: 32,
+            col: 20,
+            interval: 3.,
         }
     }
 }
 
 impl Program<Message> for Grid {
     fn draw(&self, bounds: iced::Rectangle, _cursor: Cursor) -> Vec<Geometry> {
-        let mut frame = Frame::new(bounds.size());
+        let size = bounds.size();
+
+        let mut x_offset = 0.;
+        let mut y_offset = 0.;
+        let node_size;
+
+        if size.width / size.height > self.col as f32 / self.row as f32 {
+            // 表示 canvas 的 width 比较大, 小的(即height)则铺满, x 方向有偏移
+            node_size = (size.height + self.interval) / self.row as f32 - self.interval;
+            x_offset = (size.width - (node_size  + self.interval) * self.col as f32) / 2.0;
+        } else {
+            // 表示 canvas 的 height 比较大, 小的(即width)则铺满, y 方向有偏移
+            node_size = (size.width + self.interval) / self.col as f32 - self.interval;
+            y_offset = (size.height - (node_size  + self.interval) * self.row as f32) / 2.0;
+        };
+
+        let mut frame = Frame::new(size);
         for row in 0..self.row {
-            let y_start = (self.node_size + self.interval) * (row + 1) as f32;
+            let y_start = y_offset + (node_size + self.interval) * row as f32;
             for col in 0..self.col {
-                let x_start = (self.node_size + self.interval) * (col + 1) as f32;
+                let x_start = x_offset + (node_size + self.interval) * col as f32;
                 frame.fill_rectangle(
                     Point::new(x_start, y_start),
-                    Size::new(self.node_size, self.node_size),
-                    Color::from_rgb8(0xff, 0x00, 0x00),
+                    Size::new(node_size, node_size),
+                    Color::from_rgb8(0xff, 0xd7, 0x00),
                 );
             }
         }
